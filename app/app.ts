35554efd3ac2cb499703fe17f3a0aef7137e2d4e
NicholasThrom <narthrom@edu.uwaterloo.ca>
2018-01-25 00:50:00 -0500

    Added app.ts.
    This should actually work now. The app is not very fancy.

---
+++ app/app.ts
@@ -0,0 +1,36 @@
+// The top of the application. Everything runs from here.
+
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import lessMiddleware = require("less-middleware");
+import * as path from "path";
+import { log } from "../modules/log/log";
+import { HTTPError } from "../modules/types/types";
+
+export const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, "app/public")));
+app.use("/public", express.static(path.join(__dirname, "app/public")));
+
+app.use((req, res, next) => {
+    log("Hello");
+    const error = new HTTPError("Page not found");
+    error.status = 404;
+    next(error);
+});
+
+// Typescript apparently is incapable of working out these types for itself.
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (err instanceof HTTPError) {
+        res.status(err.status);
+    } else {
+        res.status(500);
+    }
+    res.send(err.message);
+});
