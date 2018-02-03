5fca23edba40ef6f05133afb507b3c609051ba98
NicholasThrom <narthrom@edu.uwaterloo.ca>
2018-02-03 01:48:57 -0500

    Fixed top test.
    It was using the wrong assertion.

--- test/test.ts
+++ test/test.ts
@@ -2,11 +2,11 @@
 // This is the top test file. It tests that tests exist.
 //
 
+import * as chai from "chai";
 import "mocha";
-import * as sinon from "sinon";
 
 describe("Mocha tests", function () {
     it("should work", function () {
-        sinon.assert.pass(true);
+        chai.assert(true);
     });
 });
